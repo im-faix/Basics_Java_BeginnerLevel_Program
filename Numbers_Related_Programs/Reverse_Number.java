93613c0e0c005d5f9c3230645fb68709613b102b
Mohammed Faizan <156441568+im-faix@users.noreply.github.com>
2025-02-10 09:20:02 +0530

    "Reverse  a NUmber"

---
+++ Numbers_Related_Programs/Reverse_Number.java
@@ -0,0 +1,16 @@
+public class Reverse_Number
+{
+    public static void main(String[] args)
+    {
+        int n =153,rev=0;
+        System.out.println(n);
+        while(n>0)
+        {
+            int rem = n%10;
+            rev = rev *10 + rem;
+            n = n/10;
+
+        }
+        System.out.println(rev);
+    }
+}
